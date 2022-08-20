fa533ecdeb6805fd1142c486e98784ab8d4120eb
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-08-19 18:23:57 -0700

    api working now when querying for it

--- server.js
+++ server.js
@@ -3,34 +3,48 @@ const app = express();
 const port = process.env.PORT || 3000;
 const axios = require("axios");
 
-const options = {
-  method: "GET",
-  url: "https://twinword-word-associations-v1.p.rapidapi.com/associations/",
-  params: { entry: "sound" },
-  headers: {
-    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-    "X-RapidAPI-Host": "twinword-word-associations-v1.p.rapidapi.com",
-  },
-};
+// Body parameters for the API
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
-  app.get('/api/associations/:word', (req, res) => {
-    const request = unirest("GET", "https://twinword-word-associations-v1.p.rapidapi.com/associations/");
-    request.query({ "entry": req.params.word });
-    request.headers({
-      "x-rapidapi-host": "twinword-word-associations-v1.p.rapidapi.com",
-      "x-rapidapi-key": "YOUR_RAPID_API_KEY_GOES_HERE",
-      "useQueryString": true
+app.get("/api/associations/:word", (req, res) => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("indent", "yes");
+  encodedParams.append("lang", "en");
+  encodedParams.append("limit", "50");
+  encodedParams.append("pos", "noun,adjective,verb,adverb");
+  encodedParams.append("text", req.params.word); // can grab the :word in the URI to be used here
+  encodedParams.append("type", "stimulus");
+
+  const options = {
+    method: "POST",
+    url: "https://wordassociations-word-associations-v1.p.rapidapi.com/json/search",
+    params: { apikey: "606d17fc-8fc0-49e5-b351-499bea92d92b" },
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "d404a1e24fmshd8b99d017f768bep164f9djsn74105ed77c0d",
+      "X-RapidAPI-Host": "wordassociations-word-associations-v1.p.rapidapi.com",
+    },
+    data: encodedParams,
+  };
+
+  const request = axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
     });
-
+});
 
 app.get("/", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
